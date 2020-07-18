d4bcb526c05551e84f179860954c41d2d0bc716c
jameswilddev <jameswilddev@outlook.com>
2020-07-18 12:56:47 +0100

    Amend test cases.

--- change-tracking/generate-diff/unit.ts
+++ change-tracking/generate-diff/unit.ts
@@ -8,18 +8,24 @@ describe(`generateDiff`, () => {
       {
         testChangedKeyA: 250977841,
         testChangedKeyB: 498066877,
+        testUnchangedKeyC: 529788615,
+        testUnchangedKeyA: 789707642,
         testDeletedKeyB: 592434368,
         testChangedKeyC: 657809196,
         testDeletedKeyA: 786761939,
+        testUnchangedKeyB: 591733676,
         testDeletedKeyC: 670674224,
       },
       {
         testChangedKeyA: 419651368,
-        testChangedKeyB: 640184614,
+        testUnchangedKeyB: 591733676,
         testAddedKeyB: 622964471,
         testChangedKeyC: 860705509,
-        testAddedKeyA: 152522627,
+        testChangedKeyB: 640184614,
         testAddedKeyC: 842584506,
+        testUnchangedKeyA: 789707642,
+        testAddedKeyA: 152522627,
+        testUnchangedKeyC: 529788615,
       }
     );
   });
