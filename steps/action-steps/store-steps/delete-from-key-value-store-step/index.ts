73788be4bf978d0d6bd9896dda1efc23ab92c1af
jameswilddev <jameswilddev@outlook.com>
2020-07-18 17:19:08 +0100

    Add step to delete from a key value store.

---
+++ steps/action-steps/store-steps/delete-from-key-value-store-step/index.ts
@@ -0,0 +1,17 @@
+import { ActionStep } from "../../action-step";
+import { KeyValueStoreInterface } from "../../../../stores/key-value-store";
+
+export class DeleteFromKeyValueStoreStep<TKey, TValue> extends ActionStep {
+  constructor(
+    public readonly keyValueStore: KeyValueStoreInterface<TKey, TValue>,
+    public readonly key: TKey
+  ) {
+    super(
+      `Delete ${JSON.stringify(key)} from ${JSON.stringify(keyValueStore.name)}`
+    );
+  }
+
+  async execute(): Promise<void> {
+    this.keyValueStore.delete(this.key);
+  }
+}
---
+++ steps/action-steps/store-steps/delete-from-key-value-store-step/unit.ts
@@ -0,0 +1,115 @@
+import { KeyValueStoreInterface } from "../../../../stores/key-value-store";
+import { DeleteFromKeyValueStoreStep } from ".";
+
+describe(`DeleteFromKeyValueStoreStep`, () => {
+  type TestKey = `Test Key`;
+  type TestValue = `Test Value`;
+
+  describe(`on construction`, () => {
+    let keyValueStoreGet: jasmine.Spy;
+    let keyValueStoreSet: jasmine.Spy;
+    let keyValueStoreDelete: jasmine.Spy;
+    let keyValueStore: KeyValueStoreInterface<TestKey, TestValue>;
+    let deleteFromKeyValueStoreStep: DeleteFromKeyValueStoreStep<
+      TestKey,
+      TestValue
+    >;
+
+    beforeAll(() => {
+      keyValueStoreGet = jasmine.createSpy(`keyValueStoreGet`);
+      keyValueStoreSet = jasmine.createSpy(`keyValueStoreSet`);
+      keyValueStoreDelete = jasmine.createSpy(`keyValueStoreDelete`);
+      keyValueStore = {
+        name: `Test Name`,
+        get: keyValueStoreGet,
+        set: keyValueStoreSet,
+        delete: keyValueStoreDelete,
+      };
+
+      deleteFromKeyValueStoreStep = new DeleteFromKeyValueStoreStep(
+        keyValueStore,
+        `Test Key`
+      );
+    });
+
+    it(`exposes its name`, () => {
+      expect(deleteFromKeyValueStoreStep.name).toEqual(
+        `Delete "Test Key" from "Test Name"`
+      );
+    });
+
+    it(`exposes the key value store`, () => {
+      expect(deleteFromKeyValueStoreStep.keyValueStore).toBe(keyValueStore);
+    });
+
+    it(`exposes the key`, () => {
+      expect(deleteFromKeyValueStoreStep.key).toEqual(`Test Key`);
+    });
+
+    it(`does not get from the key value store`, () => {
+      expect(keyValueStoreGet).not.toHaveBeenCalled();
+    });
+
+    it(`does not set values in the key value store`, () => {
+      expect(keyValueStoreSet).not.toHaveBeenCalled();
+    });
+
+    it(`does not delete values from the key value store`, () => {
+      expect(keyValueStoreDelete).not.toHaveBeenCalled();
+    });
+  });
+
+  describe(`on execution`, () => {
+    let keyValueStoreGet: jasmine.Spy;
+    let keyValueStoreSet: jasmine.Spy;
+    let keyValueStoreDelete: jasmine.Spy;
+    let keyValueStore: KeyValueStoreInterface<TestKey, TestValue>;
+    let deleteFromKeyValueStoreStep: DeleteFromKeyValueStoreStep<
+      TestKey,
+      TestValue
+    >;
+
+    beforeAll(async () => {
+      keyValueStoreGet = jasmine.createSpy(`keyValueStoreGet`);
+      keyValueStoreSet = jasmine.createSpy(`keyValueStoreSet`);
+      keyValueStoreDelete = jasmine.createSpy(`keyValueStoreDelete`);
+      keyValueStore = {
+        name: `Test Name`,
+        get: keyValueStoreGet,
+        set: keyValueStoreSet,
+        delete: keyValueStoreDelete,
+      };
+
+      deleteFromKeyValueStoreStep = new DeleteFromKeyValueStoreStep(
+        keyValueStore,
+        `Test Key`
+      );
+
+      await deleteFromKeyValueStoreStep.execute();
+    });
+
+    it(`continues to expose the key value store`, () => {
+      expect(deleteFromKeyValueStoreStep.keyValueStore).toBe(keyValueStore);
+    });
+
+    it(`continues to expose the key`, () => {
+      expect(deleteFromKeyValueStoreStep.key).toEqual(`Test Key`);
+    });
+
+    it(`does not get from the key value store`, () => {
+      expect(keyValueStoreGet).not.toHaveBeenCalled();
+    });
+
+    it(`does not set values in the key value store`, () => {
+      expect(keyValueStoreSet).not.toHaveBeenCalled();
+    });
+
+    it(`deletes one value from the key value store`, () => {
+      expect(keyValueStoreDelete).toHaveBeenCalledTimes(1);
+    });
+
+    it(`deletes the specified value from the key value store`, () => {
+      expect(keyValueStoreDelete).toHaveBeenCalledWith(`Test Key`);
+    });
+  });
+});
