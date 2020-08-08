962fb20b25dfa820b22c5c0d152ac31083090dc7
jameswilddev <jameswilddev@outlook.com>
2020-08-08 17:54:00 +0100

    Add a function to map the items in a diff.

---
+++ change-tracking/map-diff/index.ts
@@ -0,0 +1,36 @@
+import { Diff } from "../diff";
+
+export const mapDiff = <TInput, TOutput>(
+  diff: Diff<TInput>,
+  callback: (input: TInput) => null | TOutput
+): {
+  readonly diff: Diff<TOutput>;
+  readonly errors: ReadonlyArray<TInput>;
+} => {
+  const errors: TInput[] = [];
+  const added: TOutput[] = [];
+
+  for (const input of diff.added) {
+    const mapped = callback(input);
+
+    if (mapped === null) {
+      errors.push(input);
+    } else {
+      added.push(mapped);
+    }
+  }
+
+  const changed = diff.changed
+    .map(item => callback(item))
+    .filter((mapped): mapped is TOutput => mapped !== null);
+
+  const deleted = diff.deleted
+  .map(item => callback(item))
+    .filter((mapped): mapped is TOutput => mapped !== null);
+
+  const unchanged = diff.unchanged
+  .map(item => callback(item))
+    .filter((mapped): mapped is TOutput => mapped !== null);
+
+  return { diff: { added, changed, deleted, unchanged }, errors };
+};
