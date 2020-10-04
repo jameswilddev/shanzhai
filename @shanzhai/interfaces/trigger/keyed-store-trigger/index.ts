13697d1b9936196773797ecb2573297b26cb69e4
jameswilddev <jameswilddev@outlook.com>
2020-10-04 09:31:25 +0100

    Add missing field.

--- @shanzhai/interfaces/trigger/keyed-store-trigger/index.ts
+++ @shanzhai/interfaces/trigger/keyed-store-trigger/index.ts
@@ -4,6 +4,8 @@ import { Store } from "../../store";
 export type KeyedStoreTrigger = {
   readonly type: `keyedStore`;
 
+  readonly store: Store;
+
   readonly regeneratedByChangesToStores: ReadonlyArray<Store>;
 
   down(key: string): Promise<ReadonlyArray<Step>>;
