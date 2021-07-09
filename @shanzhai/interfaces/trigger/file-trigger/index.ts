d70c7fa7e1a2275fed58fd446ca4b8078ae08b28
jameswilddev <jameswilddev@outlook.com>
2021-07-09 16:32:53 +0100

    Remove regeneration from file triggers.

--- @shanzhai/interfaces/trigger/file-trigger/index.ts
+++ @shanzhai/interfaces/trigger/file-trigger/index.ts
@@ -1,12 +1,10 @@
 import { ParsedPath } from "../../parsed-path";
 import { Step } from "../../step";
-import { Store } from "../../store";
 
 export type FileTrigger = {
   readonly type: `file`;
 
   readonly extension: string;
-  readonly regeneratedByChangesToStore: Store;
 
   down(path: ParsedPath): Step;
   up(path: ParsedPath): Step;
