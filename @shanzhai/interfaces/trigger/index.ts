ac1b48e77576e566e2bbb3a16016c1252b60ff09
jameswilddev <jameswilddev@outlook.com>
2021-07-26 00:18:02 +0100

    Add missing trigger.

--- @shanzhai/interfaces/trigger/index.ts
+++ @shanzhai/interfaces/trigger/index.ts
@@ -1,10 +1,12 @@
 import { FileTrigger } from "./file-trigger";
 import { KeyedStoreTrigger } from "./keyed-store-trigger";
 import { OneTimeTrigger } from "./one-time-trigger";
+import { StoreAggregateTrigger } from "./store-aggregate-trigger";
 import { UnkeyedStoreTrigger } from "./unkeyed-store-trigger";
 
 export type Trigger =
   | FileTrigger
   | KeyedStoreTrigger
   | OneTimeTrigger
-  | UnkeyedStoreTrigger;
+  | UnkeyedStoreTrigger
+  | StoreAggregateTrigger;
