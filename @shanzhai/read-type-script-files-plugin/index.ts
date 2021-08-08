088f1ab0a148a618358cfda82f2b7134dda3be16
jameswilddev <jameswilddev@outlook.com>
2021-08-08 21:46:54 +0100

    Add a plugin to read type script files.

---
+++ @shanzhai/read-type-script-files-plugin/index.ts
@@ -0,0 +1,37 @@
+import {
+  Plugin,
+  FileExtensionTrigger,
+  ParsedPath,
+  Step,
+} from "@shanzhai/interfaces";
+import {
+  DeleteFromKeyValueStoreStep,
+  KeyValueStoreOutput,
+} from "@shanzhai/key-value-store";
+import { ReadTextFileStep } from "@shanzhai/read-text-file-step";
+import { typeScriptSourceStore } from "@shanzhai/type-script-source-store";
+
+const readTypeScriptFilesPlugin: Plugin<{
+  readonly readTypeScriptFiles: FileExtensionTrigger;
+}> = {
+  triggers: {
+    readTypeScriptFiles: {
+      type: `fileExtension`,
+      extension: `ts`,
+      down(path: ParsedPath): Step {
+        return new DeleteFromKeyValueStoreStep(
+          typeScriptSourceStore,
+          path.fullPath
+        );
+      },
+      up(path: ParsedPath): Step {
+        return new ReadTextFileStep(
+          [path.fullPath],
+          new KeyValueStoreOutput(typeScriptSourceStore, path.fullPath)
+        );
+      },
+    },
+  },
+};
+
+export default readTypeScriptFilesPlugin;
---
+++ @shanzhai/read-type-script-files-plugin/unit.ts
@@ -0,0 +1,55 @@
+import { Step } from "@shanzhai/interfaces";
+import { DeleteFromKeyValueStoreStep } from "@shanzhai/key-value-store";
+import { typeScriptSourceStore } from "@shanzhai/type-script-source-store";
+import { ReadTextFileStep } from "@shanzhai/read-text-file-step";
+import { KeyValueStoreOutput } from "@shanzhai/key-value-store";
+import readTypeScriptFilesPlugin from ".";
+
+describe(`read-type-script-files-plugin`, () => {
+  it(`matches *.ts files`, () => {
+    expect(
+      readTypeScriptFilesPlugin.triggers.readTypeScriptFiles.extension
+    ).toEqual(`ts`);
+  });
+
+  describe(`when a file is removed`, () => {
+    let step: Step;
+
+    beforeAll(() => {
+      step = readTypeScriptFilesPlugin.triggers.readTypeScriptFiles.down({
+        typeScriptName: `Test TypeScript Name`,
+        fullPath: `Test Full Path`,
+        fileExtension: `Test File Extension`,
+        fullPathWithoutExtension: `Test Full Path Without Extension`,
+      });
+    });
+
+    it(`deletes the read file from the store`, () => {
+      expect(step).toEqual(
+        new DeleteFromKeyValueStoreStep(typeScriptSourceStore, `Test Full Path`)
+      );
+    });
+  });
+
+  describe(`when a file is added`, () => {
+    let step: Step;
+
+    beforeAll(() => {
+      step = readTypeScriptFilesPlugin.triggers.readTypeScriptFiles.up({
+        typeScriptName: `Test TypeScript Name`,
+        fullPath: `Test Full Path`,
+        fileExtension: `Test File Extension`,
+        fullPathWithoutExtension: `Test Full Path Without Extension`,
+      });
+    });
+
+    it(`deletes the read file from the store`, () => {
+      expect(step).toEqual(
+        new ReadTextFileStep(
+          [`Test Full Path`],
+          new KeyValueStoreOutput(typeScriptSourceStore, `Test Full Path`)
+        )
+      );
+    });
+  });
+});
