7e722e62c59a65d4c333bad43f7460d37b8c5666
jameswilddev <jameswilddev@outlook.com>
2021-08-08 20:52:13 +0100

    Add missing shebang.

--- @shanzhai/production-cli/index.ts
+++ @shanzhai/production-cli/index.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import { searchForPlugins } from "@shanzhai/plugin-helpers";
 import { plan } from "@shanzhai/planning-helpers";
 import { execute } from "@shanzhai/execution-helpers";
