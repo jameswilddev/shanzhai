87703416df9762a05aa5342039f95309ae7d4d62
jameswilddev <jameswilddev@outlook.com>
2021-08-15 21:55:34 +0100

    The global store is now a two-level hierarchy.

--- @shanzhai/global-store/index.ts
+++ @shanzhai/global-store/index.ts
@@ -1,6 +1,6 @@
 import { Json } from "@shanzhai/interfaces";
 import { KeyValueStore } from "@shanzhai/key-value-store";
 
-export const globalStore = new KeyValueStore<{ readonly [name: string]: Json }>(
-  `globalStore`
-);
+export const globalStore = new KeyValueStore<{
+  readonly [originPluginKey: string]: { readonly [globalKey: string]: Json };
+}>(`globalStore`);
