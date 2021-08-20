1c1fc37f1ed3e8e799afadb32124be3d76bb2cc4
jameswilddev <jameswilddev@outlook.com>
2021-08-20 21:49:14 +0100

    Fix type.

--- @shanzhai/pug-source-store/index.ts
+++ @shanzhai/pug-source-store/index.ts
@@ -1,3 +1,3 @@
-import { ValueStore } from "@shanzhai/value-store";
+import { KeyValueStore } from "@shanzhai/key-value-store";
 
-export const pugSourceStore = new ValueStore<string>(`pugSourceStore`);
+export const pugSourceStore = new KeyValueStore<string>(`pugSourceStore`);
