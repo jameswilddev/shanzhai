9bd2a25a53610456017e446c41ad471eb67c37f3
jameswilddev <jameswilddev@outlook.com>
2021-08-27 20:34:22 +0100

    Add list of stores which cause a keyed store trigger to refresh all.

--- @shanzhai/interfaces/trigger/keyed-store-trigger/index.ts
+++ @shanzhai/interfaces/trigger/keyed-store-trigger/index.ts
@@ -1,3 +1,4 @@
+import { UnkeyedStore } from "../../stores/unkeyed-store";
 import { KeyedStore } from "../../stores/keyed-store";
 import { Step } from "../../step";
 
@@ -5,6 +6,10 @@ export type KeyedStoreTrigger = {
   readonly type: `keyedStore`;
 
   readonly keyedStore: KeyedStore<unknown>;
+
+  readonly refreshAllWhenStoresChange: ReadonlyArray<
+    UnkeyedStore<unknown> | KeyedStore<unknown>
+  >;
 
   down(key: string): Step;
   up(key: string): Step;
