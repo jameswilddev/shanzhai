26a3c7617ff06dc6ef7631ff457255bd5778f21c
jameswilddev <jameswilddev@outlook.com>
2021-09-11 15:16:14 +0100

    Update dependencies.

--- @shanzhai/change-tracking-helpers/index.ts
+++ @shanzhai/change-tracking-helpers/index.ts
@@ -1,5 +1,3 @@
 export { generateDiff } from "./generate-diff";
-export { mapDiff } from "./map-diff";
-export { parsePath } from "./parse-path";
 export { pathAccepted } from "./path-accepted";
 export { hashFile } from "./hash-file";
