563493c0266fa9512cd8b38a4e32d7e343b41833
jameswilddev <jameswilddev@outlook.com>
2022-04-02 23:21:48 +0100

    Improve publish performance.

--- publish/process-all.ts
+++ publish/process-all.ts
@@ -3,7 +3,9 @@ import processPackage from "./process-package";
 
 export default async function (): Promise<void> {
   console.log(`Publishing namespaced packages...`);
-  for (const name of await fs.promises.readdir(`@shanzhai`)) {
-    await processPackage([`@shanzhai`, name]);
-  }
+  await Promise.all(
+    (
+      await fs.promises.readdir(`@shanzhai`)
+    ).map((name) => processPackage([`@shanzhai`, name]))
+  );
 }
