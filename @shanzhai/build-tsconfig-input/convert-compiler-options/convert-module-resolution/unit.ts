8cfd757f9d8fd6c1b6cc7fd9e37dc9e3124f9816
jameswilddev <jameswilddev@outlook.com>
2022-04-02 22:16:09 +0100

    Update dependencies.

--- @shanzhai/build-tsconfig-input/convert-compiler-options/convert-module-resolution/unit.ts
+++ @shanzhai/build-tsconfig-input/convert-compiler-options/convert-module-resolution/unit.ts
@@ -4,10 +4,10 @@ import { convertModuleResolution } from ".";
 describe(`convert-module-resolution`, () => {
   function maps(
     from: undefined | ModuleResolutionKind,
-    to: undefined | `Classic` | `Node`
+    to: undefined | `Classic` | `Node` | `Node12` | `NodeNext`
   ): void {
     describe(`given ${from}`, () => {
-      let output: undefined | `Classic` | `Node`;
+      let output: undefined | `Classic` | `Node` | `Node12` | `NodeNext`;
 
       beforeAll(() => {
         output = convertModuleResolution(from);
@@ -22,4 +22,6 @@ describe(`convert-module-resolution`, () => {
   maps(undefined, undefined);
   maps(ModuleResolutionKind.Classic, `Classic`);
   maps(ModuleResolutionKind.NodeJs, `Node`);
+  maps(ModuleResolutionKind.Node12, `Node12`);
+  maps(ModuleResolutionKind.NodeNext, `NodeNext`);
 });
